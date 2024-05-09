57ded81b3b8bca8ac11e46c256bea2f7918c8f77
vladyusa <usatenkovladislav33@gmail.com>
2024-05-09 11:27:55 +0200

    8. dodałem annotacje @Override w klasie HanoiRhymer dla metody countIn

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -3,11 +3,11 @@ package edu.kis.vh.nursery;
 public class HanoiRhymer extends defaultCountingOutRhymer {
     private static final int CONSTANT_ZERO = 0;
 	int totalRejected = CONSTANT_ZERO;
-
+	
     public int reportRejected() {
         return totalRejected;
     }
-
+    @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
